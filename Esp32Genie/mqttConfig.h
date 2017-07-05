7587080dd9d2173da7399cefec615bbf0fae2b02
David Wallis <david@wallis2000.co.uk>
2017-07-05 23:51:29 +0100

    sync and add extra files from splitting out  with '#' will be ignored, and an empty message aborts the commit.

---
+++ Esp32Genie/mqttConfig.h
@@ -0,0 +1,9 @@
+// MQTT Settings
+#define mqtt_server "192.168.0.161"
+#define mqtt_server_port 1883
+//#define mqtt_user ""
+//#define mqtt_password ""
+#define base_topic "home/espgenie"
+#define temperature_topic "/sensor/temp"
+#define wifi_topic "/sensor/signal"
+
---
+++ Esp32Genie/pinMappings.h
@@ -0,0 +1,5 @@
+// Define pin mappings
+#define ONE_WIRE_BUS 2
+#define RELAY_PIN 5
+#define SWITCH_PIN 13
+
